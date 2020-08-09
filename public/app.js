7f41ac49b16f8c1c11f326c71405e60c3e1978dc
Emma McDonald-Brown <e.mcdonald-brown@outlook.com>
2020-08-09 19:45:41 +0100

    weather app working - pre long lat

---
+++ public/app.js
@@ -0,0 +1,53 @@
+const api = {
+    key: "3f970c61f1cb64e3a84ba951b8152214",
+    baseurl: "https://api.openweathermap.org/data/2.5/",
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp).toFixed(0)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min).toFixed(0)}°c / ${Math.round(weather.main.temp_max).toFixed(0)}°c`;
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+"Saturday", "Sunday"];
+
+let day = days[d.getDay()];
+let date = d.getDate();
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+return `${day} ${date} ${month} ${year}`;
+}   
